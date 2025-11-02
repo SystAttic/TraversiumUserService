8ac157324d581b6fdb588999432f788483f6e77d
Maja Razinger <maja.razinger@better.care>
2025-11-02 10:26:45 +0100

    Add created at for users

--- src/main/kotlin/travesium/userservice/dto/UserDto.kt
+++ src/main/kotlin/travesium/userservice/dto/UserDto.kt
@@ -1,5 +1,7 @@
 package travesium.userservice.dto
 
+import java.time.OffsetDateTime
+
 /**
  * @author Maja Razinger
  */
@@ -10,6 +12,7 @@ data class UserDto(
     val displayName: String? = username,
     val avatarPhotoReference: String? = null,
     val coverPhotoReference: String? = null,
+    val createdAt: OffsetDateTime = OffsetDateTime.now(),
     val description: String? = null,
     val following: Set<UserDto> = emptySet(),
     val followers: Set<UserDto> = emptySet(),
