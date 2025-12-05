40a657fe51fd7b9342893f8233daf6fb0f351352
Maja Razinger <maja.razinger@better.care>
2025-12-05 21:29:41 +0100

    add tenant to expose endpoint

---
+++ src/main/kotlin/travesium/userservice/security/TenantFilter.kt
@@ -0,0 +1,45 @@
+package travesium.userservice.security
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import traversium.commonmultitenancy.TenantContext
+import traversium.commonmultitenancy.TenantUtils
+
+/**
+ * @author Maja Razinger
+ */
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class TenantFilter : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        try {
+            val tenantId = request.getHeader("X-Tenant-Id")
+            val sanitizedTenantId = TenantUtils.sanitizeTenantIdForSchema(tenantId ?: "public")
+            TenantContext.setTenant(sanitizedTenantId)
+
+            filterChain.doFilter(request, response)
+        } finally {
+            TenantContext.clear()
+        }
+    }
+
+    override fun shouldNotFilter(request: HttpServletRequest): Boolean {
+        val path = request.requestURI.removePrefix(request.contextPath)
+
+        val unauthenticatedPaths = setOf(
+            "/rest/v1/users/exists"
+        )
+
+        return path !in unauthenticatedPaths
+    }
+}
